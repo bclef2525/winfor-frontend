508a7c2e87a1f0ec1cbced1c9a7744e884ccc3b3
jungsw <jungsw586@gmail.com>
2019-10-28 17:39:09 +0900

    add Components, Styles folder and StaticsPages

--- src/Routes.js
+++ src/Routes.js
@@ -7,6 +7,8 @@ import {
 import Main from './Pages/Main/';
 import Signup from './Pages/Signup/';
 import Signin from './Pages/Login/';
+import StaticsChampions from './Pages/StaticsChampions';
+import StaticsTiers from './Pages/StaticsTiers';
 
 class Routes extends React.Component {
   render() {
@@ -16,6 +18,8 @@ class Routes extends React.Component {
           <Route exact path="/" component={Main} />
           <Route exact path="/Signup" component={Signup} />
           <Route exact path="/Login" component={Signin} />
+          <Route exact path="/Statics/Champions" component={StaticsChampions} />
+          <Route exact path="/Statics/Tiers" component={StaticsTiers} />
         </Switch>
       </Router>
     )
