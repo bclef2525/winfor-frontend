b312d7b2104f20cb633979c29416bdf902af7a2f
macbookpro <bclef25@macbookproui-MacBookPro.local>
2019-10-30 22:10:42 +0900

    메인페이지 해더 수정

--- src/Pages/Main/MainHeader/MainHeader.js
+++ src/Pages/Main/MainHeader/MainHeader.js
@@ -5,24 +5,32 @@ class MainHeader extends Component {
   render() {
     return (
       <div className="main-header">
-        <div className="main-logo">Winfor.GG</div>
         <div className="main-nav">
+          <div className="main-logo">Winfor.GG</div>
           <div className="main-nav-tab">
             <Link to={"/"} className="main-nav-home">
-              홈
+              홈<div className="under-line" />
             </Link>
             <Link to={"/Statics"} className="main-nav-static">
               통계
+              <div className="under-line" />
             </Link>
-            <Link className="main-nav-rank">랭킹</Link>
-            <Link className="main-nav-comunity">커뮤니티</Link>
+            <Link className="main-nav-rank">
+              랭킹
+              <div className="under-line" />
+            </Link>
+            <Link className="main-nav-comunity">
+              커뮤니티
+              <div className="under-line" />
+            </Link>
           </div>
           <div className="main-nav-login-box">
-            <Link to={"/login"} className="login-box-login">
-              로그인
-            </Link>
             <Link to={"/signup"} className="login-box-signup">
               회원가입
+              <div className="under-line" />
+            </Link>
+            <Link to={"/login"} className="login-box-login">
+              로그인
             </Link>
           </div>
         </div>
