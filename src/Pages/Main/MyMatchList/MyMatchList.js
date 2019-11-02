5d4957126edbe3643b03b62dfbdff381e6a444a0
macbookpro <bclef25@macbookproui-MacBookPro.local>
2019-11-02 23:05:01 +0900

    make myMatchList page

---
+++ src/Pages/Main/MyMatchList/MyMatchList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import MainHeader from "../MainHeader/MainHeader";
+export class MyMatchList extends Component {
+  render() {
+    return (
+      <div className="mml-page">
+        <MainHeader />
+      </div>
+    );
+  }
+}
+
+export default MyMatchList;
