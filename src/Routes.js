c956f1bd0028cd9b22782d4fb1ea7ab5f1add65b
macbookpro <bclef25@macbookproui-MacBookPro.local>
2019-11-04 13:45:16 +0900

    라우터 수정

--- src/Routes.js
+++ src/Routes.js
@@ -5,7 +5,7 @@ import Signup from "./Pages/Signup/";
 import Signin from "./Pages/Login/";
 import StaticsChampions from "./Pages/StaticsChampions";
 import StaticsTiers from "./Pages/StaticsTiers";
-import MyMatchList from "./Pages/Main/MyMatchList/MyMatchList";
+import MyMatchList from "./Pages/MyMatchList/MyMatchList";
 
 class Routes extends React.Component {
   render() {
