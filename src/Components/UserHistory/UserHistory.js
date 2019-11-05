4056d1fcbf47fc7d8c170f67850f4d6ba0d86e80
sehwanforeal <55143030+sehwanforeal@users.noreply.github.com>
2019-11-06 01:09:41 +0900

    mockupdata 완성후 mount완성

--- src/Components/UserHistory/UserHistory.js
+++ src/Components/UserHistory/UserHistory.js
@@ -1,55 +1,188 @@
 import React, { Component } from "react";
 import "./UserHistoru.scss";
-import Players from "../UserHistory/Players/Players";
 
 export class UserHistory extends Component {
   render() {
+    let { index } = this.props;
+    let { info } = this.props;
+    let kills = () => {
+      if (info.game[0].pentakill !== 0) {
+        return "펜타킬";
+      } else if (info.game[0].quadrakill !== 0) {
+        return "쿼드라킬";
+      } else if (info.game[0].triplekill !== 0) {
+        return "트리플킬";
+      } else if (info.game[0].doublekill !== 0) {
+        return "더블킬";
+      } else {
+        return "싱글킬";
+      }
+    };
+    console.log(kills);
+    console.log(this.props.info.game[0].id);
     return (
       <>
         <div className="mml-user-history-container">
           <div className="mml-user-history">
             <div className="mml-user-game-stats">
-              <div className="stats-game-type">무작위 총력전</div>
-              <div className="stats-game-day">7일 전</div>
+              <div className="stats-game-type">{info.game[0].gametype}</div>
+              <div className="stats-game-day">{info.game[0].start_time}</div>
               <div className="border"></div>
-              <div className="stats-game-result">승리</div>
-              <div className="stats-game-time">19분 22초</div>
+              <div className="stats-game-result">{info.game[0].win}</div>
+              <div className="stats-game-time">{info.game[0].gameduration}</div>
             </div>
             <div className="mml-user-setting">
-              <div className="setting-champ-image"></div>
+              <img
+                src={info.game[0].to_user1_champion[0].champion_img_src}
+                className="setting-champ-image"
+              ></img>
               <div className="setting-champ-skill-container">
-                <div className="setting-champ-skills"></div>
-                <div className="setting-champ-skills"></div>
-                <div className="setting-champ-skills"></div>
-                <div className="setting-champ-skills"></div>
+                <img
+                  src={info.game[0].spell1[0].spell_img_src}
+                  className="setting-champ-skills thespell"
+                ></img>
+                <img
+                  src={info.game[0].sub_rune[0].subrune_img_src}
+                  className="setting-champ-skills"
+                ></img>
+
+                <img
+                  src={info.game[0].spell2[0].spell_img_src}
+                  className="setting-champ-skills thespell"
+                ></img>
+                <img
+                  src={info.game[0].core_rune[0].rune_img_src}
+                  className="setting-champ-skills"
+                ></img>
               </div>
-              <div className="setting-champ-name">애쉬</div>
+              <div className="setting-champ-name">
+                {info.game[0].to_user1_champion[0].champion_name}
+              </div>
             </div>
             <div className="mml-user-KDA">
               <div className="KDA-container">
-                <div className="KDA-kill">10</div>/
-                <div className="KDA-death">7</div>/
-                <div className="KDA-assist">22</div>
+                <div className="KDA-kill">{info.game[0].kill}</div>/
+                <div className="KDA-death">{info.game[0].death}</div>/
+                <div className="KDA-assist">{info.game[0].assist}</div>
               </div>
               <div className="KDA-ratio">
-                <div className="KDA-ratio-potint"> 4.57:1 평점</div>
+                <div className="KDA-ratio-potint">
+                  {(
+                    (info.game[0].kill + info.game[0].assist) /
+                    info.game[0].death
+                  ).toFixed(2)}
+                  :1 평점
+                </div>
               </div>
-              <div className="kill-perfomence">더블킬</div>
+              <div className="kill-perfomence">
+                {/* {() => {
+                  if (info.game[0].pentakill > 0) {
+                    return "nopenta";
+                  }
+                }} */}
+                {kills()}
+              </div>
             </div>
             <div className="mml-user-stats">
               <div className="user-stats-level">레벨 18</div>
-              <div className="user-stats-CS">69 (3.3) CS</div>
-              <div className="user-stats-engage">킬관여 82%</div>
+              <div className="user-stats-CS">{info.game[0].cs} CS</div>
+              <div className="user-stats-engage">분당CS 8.3</div>
             </div>
             <div className="mml-user-players">
               <div className="players-my-team">
-                <Players />
-                <Players /> <Players /> <Players /> <Players />
+                <div className="players-container">
+                  <img
+                    src={info.game[0].champions[0].champion_img_src}
+                    className="player-image"
+                  ></img>
+                  <div className="player-name">
+                    {info.game[0].user[0].user_name}
+                  </div>
+                </div>
+                <div className="players-container">
+                  <img
+                    src={info.game[0].to_user1_champion[0].champion_img_src}
+                    className="player-image"
+                  ></img>
+                  <div className="player-name">
+                    {info.game[0].to_user1_name}
+                  </div>
+                </div>
+                <div className="players-container">
+                  <img
+                    src={info.game[0].to_user2_champion[0].champion_img_src}
+                    className="player-image"
+                  ></img>
+                  <div className="player-name">
+                    {info.game[0].to_user2_name}
+                  </div>
+                </div>
+                <div className="players-container">
+                  <img
+                    src={info.game[0].to_user3_champion[0].champion_img_src}
+                    className="player-image"
+                  ></img>
+                  <div className="player-name">
+                    {info.game[0].to_user3_name}
+                  </div>
+                </div>
+                <div className="players-container">
+                  <img
+                    src={info.game[0].to_user4_champion[0].champion_img_src}
+                    className="player-image"
+                  ></img>
+                  <div className="player-name">
+                    {info.game[0].to_user4_name}
+                  </div>
+                </div>
               </div>
 
               <div className="players-enemy-team">
-                {" "}
-                <Players /> <Players /> <Players /> <Players /> <Players />
+                <div className="players-container">
+                  <img
+                    src={info.game[0].en_user1_champion[0].champion_img_src}
+                    className="player-image"
+                  ></img>
+                  <div className="player-name">
+                    {info.game[0].en_user1_name}
+                  </div>
+                </div>
+                <div className="players-container">
+                  <img
+                    src={info.game[0].en_user2_champion[0].champion_img_src}
+                    className="player-image"
+                  ></img>
+                  <div className="player-name">
+                    {info.game[0].en_user2_name}
+                  </div>
+                </div>
+                <div className="players-container">
+                  <img
+                    src={info.game[0].en_user3_champion[0].champion_img_src}
+                    className="player-image"
+                  ></img>
+                  <div className="player-name">
+                    {info.game[0].en_user3_name}
+                  </div>
+                </div>
+                <div className="players-container">
+                  <img
+                    src={info.game[0].en_user4_champion[0].champion_img_src}
+                    className="player-image"
+                  ></img>
+                  <div className="player-name">
+                    {info.game[0].en_user4_name}
+                  </div>
+                </div>
+                <div className="players-container">
+                  <img
+                    src={info.game[0].en_user5_champion[0].champion_img_src}
+                    className="player-image"
+                  ></img>
+                  <div className="player-name">
+                    {info.game[0].en_user5_name}
+                  </div>
+                </div>
               </div>
             </div>
             <div className="history-boast">자랑하기</div>
--- src/Pages/MyMatchList/MyMatchList.js
+++ src/Pages/MyMatchList/MyMatchList.js
@@ -2,42 +2,53 @@ import React, { Component } from "react";
 import MainHeader from "../Main/MainHeader/MainHeader";
 import "./MyMatchList.scss";
 import UserHistory from "../../Components/UserHistory/UserHistory";
+import mmlMock from "./MyMatchListMock";
+
 export class MyMatchList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mmldata: mmlMock
+    };
+  }
   render() {
+    console.log(this.state.mmldata);
     return (
       <div className="mml-page">
         <MainHeader />
         <div className="mml-view-page">
           <div className="mml-header">
             <div className="mml-user-info">
-              <div className="mml-rank-tag-container">
+              {/* <div className="mml-rank-tag-container">
                 <div className="mml-rank-tag">S5 Silver</div>
                 <div className="mml-rank-tag">S6 Bronze</div>
                 <div className="mml-rank-tag">S7 Bronze</div>
                 <div className="mml-rank-tag">S8 Silver</div>
-              </div>
+              </div> */}
               <div className="mml-user-container">
                 <div className="mml-user-image"></div>
                 <div className="mml-user-box">
-                  <div className="mml-user-name">조인호</div>
+                  <div className="mml-user-name">
+                    {this.state.mmldata[0].game[0].user[0].user_name}
+                  </div>
                   <div className="mml-user-rank">
-                    레더랭킹 1,247,872위 (상위 42%)
+                    {this.state.mmldata[0].tier}
                   </div>
                   <div className="mml-user-button">
-                    <div className="mml-user-rank-refresh">전적갱신</div>
-                    <div className="mml-user-rank-graph">티어 그래프</div>
+                    <button className="mml-user-rank-refresh">전적갱신</button>
+                    <button className="mml-user-rank-graph">
+                      전체티어통계
+                    </button>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <UserHistory />
-          <UserHistory />
-          <UserHistory />
-          <UserHistory />
-          <UserHistory />
-          <UserHistory />
-          <UserHistory />
+          {/* <UserHistory /> */}
+          {this.state.mmldata.map((el, idx) => {
+            console.log(el);
+            return <UserHistory info={el} index={idx} />;
+          })}
         </div>
       </div>
     );
